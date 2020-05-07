9a31706ccac202ce07e80bbabe078ce1f978db89
J.D <juan.ramos15@upr.edu>
2020-05-07 17:12:00 -0400

    Progress

---
+++ src/AntlrParser.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+public class AntlrParser {
+    public static void main (String[] args) throws IOException {
+        CharStream charStream = CharStreams.fromFileName("test");
+        EasyJLexer easyJLexer = new EasyJLexer(charStream);
+        CommonTokenStream commonTokenStream = new CommonTokenStream((easyJLexer));
+        EasyJParser easyJParser = new EasyJParser(commonTokenStream);
+
+        System.out.println(easyJLexer.getAllTokens());
+        
+
+        
+    }
+}
